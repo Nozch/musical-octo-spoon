21baed3bd2e6f3d9f57991c6361428e52a0db378
Nozch <freegia@gmail.com>
2023-07-22 11:28:16 +0900

    created app's crud method

--- app.go
+++ app.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-
+	"net/http"
+	"strconv"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
@@ -24,5 +26,131 @@ func (a *App) Initialize(user, password, dbname string) {
 	}
 	a.Router = mux.NewRouter()
  }
+ func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id, err := strconv.Atoi(vars["id"])
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+        return
+    }
+
+    p := product{ID: id}
+    if err := p.getProduct(a.DB); err != nil {
+        switch err {
+        case sql.ErrNoRows:
+            respondWithError(w, http.StatusNotFound, "Product not found")
+        default:
+            respondWithError(w, http.StatusInternalServerError, err.Error())
+        }
+        return
+    }
+
+    respondWithJSON(w, http.StatusOK, p)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+    respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    response, _ := json.Marshal(payload)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(response)
+}
+
+//countとstartをクエリから使う
+//start, countがない時は取得数10, 開始位置0にする
+//エラー: 製品を取得するとき
+func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
+    count, _ := strconv.Atoi(r.FormValue("count"))
+    start, _ := strconv.Atoi(r.FormValue("start"))
+
+    if count > 10 || count < 1 {
+        count = 10
+    }
+    if start < 0 {
+        start = 0
+    }
+
+    products, err := getProducts(a.DB, start, count)
+    if err != nil {
+        respondWithError(w, http.StatusInternalServerError, err.Error())
+        return
+    }
+
+    respondWithJSON(w, http.StatusOK, products)
+}
+
+//リクエストのボディにJSONを仮定
+//エラー1. リクエストの不正
+//エラー2. pのメソッドでエラー -> 内部エラー
+func (a *App) createProduct(w http.ResponseWriter, r *http.Request) {
+    var p product
+    decoder := json.NewDecoder(r.Body)
+    if err := decoder.Decode(&p); err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+        return
+    }
+    defer r.Body.Close()
+
+    if err := p.createProduct(a.DB); err != nil {
+        respondWithError(w, http.StatusInternalServerError, err.Error())
+        return
+    }
+
+    respondWithJSON(w, http.StatusCreated, p)
+}
+
+//リクエストの詳細をVarsでもらう
+//エラー1. リクエストが不正 Vars, Atoi
+//エラー2. JSONのデコード　NewDecoder, Decode
+//エラー3. 商品の更新 
+
+func (a *App) updateProduct(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id, err := strconv.Atoi(vars["id"])
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+        return
+    }
+
+    var p product
+    decoder := json.NewDecoder(r.Body)
+    if err := decoder.Decode(&p); err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
+        return
+    }
+    defer r.Body.Close()
+    p.ID = id
+
+    if err := p.updateProduct(a.DB); err != nil {
+        respondWithError(w, http.StatusInternalServerError, err.Error())
+        return
+    }
+
+    respondWithJSON(w, http.StatusOK, p)
+}
+
+//エラー1. リクエスト不正(Vars, Atoi)
+//エラー２. 商品を削除(modelの関数)
+
+func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id, err := strconv.Atoi(vars["id"])
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid Product ID")
+        return
+    }
+
+    p := product{ID: id}
+    if err := p.deleteProduct(a.DB); err != nil {
+        respondWithError(w, http.StatusInternalServerError, err.Error())
+        return
+    }
+
+    respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
 
 func (a *App) Run(addr string) { }
